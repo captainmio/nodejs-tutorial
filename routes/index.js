9ca09b02acb08a998b1bf086cd5d1ffd3d5bed7b
Rustum Jordan <rustumjordan@gmail.com>
2023-05-26 21:57:32 +0800

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// set all the routes in this application
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = router;
